a1473be99f9af5527be09b956b03b7c36b1d8531
KyanDamiaens <kyan.deprins@student.ehb.be>
2026-02-19 10:20:57 +0100

    hallo

--- 01-interactive-bounce/main.js
+++ 01-interactive-bounce/main.js
@@ -1,9 +1,13 @@
 let canvasElement = document.querySelector("#canvas");
 
 //1. Add variables needed for animating the ball
+let x = 300;
+let y = 300;
 
 //2. Add variables (size, speed, colorVal) for controlling the sketch
-
+let size = 50;
+let speed = 10;
+let colorval = "#f100";
 
 function setup() {
     //Creating a canvas to draw on, using the existing canvas element.
@@ -19,10 +23,17 @@ function draw() {
     // 3.2 Bounce logic: check the edges
 
     // 3.3 Draw
+    fill(colorval);
+    circle(x,y,size);
 }
 
 // --- DOM Interaction ---
 
 // 4. Select the input elements
+let sizeInput = document.querySelector("#size-input");
+
 
 // 5. Add Event Listeners; listen to the "input" event for real-time updates while dragging
+sizeInput.addEventListener("input", function() {
+size = Number(sizeInput.value);
+});
